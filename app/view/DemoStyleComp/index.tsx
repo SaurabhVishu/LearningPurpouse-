a688ed43fda4fa999960b5a6860f76d9cdc39fc2
Saurabh Vishwkarma <saurabh.vishwkarma@successive.tech>
2023-03-20 15:05:52 +0530

    new code

---
+++ app/view/DemoStyleComp/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Demo = () => {
+  return (
+    <View>
+      <Text>Hiiii</Text>
+    </View>
+  );
+};
+export default Demo;
--- app/view/RandomScreen/index.tsx
+++ app/view/RandomScreen/index.tsx
@@ -1,33 +1,26 @@
-import React from "react";
-import {View,Text, SafeAreaView} from "react-native"
-import {WebView} from "react-native-webview"
+import React from 'react';
+import {View, SafeAreaView} from 'react-native';
+import {WebView} from 'react-native-webview';
 
+import styles from './style';
 
-import styles from "./style";
-
-
- interface RandomScreenProps{
-    navigation:any
+interface RandomScreenProps {
+  navigation: any;
 }
 
-
-const RandomScreen=(props:RandomScreenProps)=>{
-    const{navigation}=props
-    return(
-        <SafeAreaView style={{flex:1}}>
-            <View style={{backgroundColor:"red"}}>
-               
-            </View>
-            <View style={styles.mainContainer}>
-            <WebView
-        source={{
-          uri: 'https://www.w3schools.com'
-        }}
-      />
-            </View>
-        </SafeAreaView>
-    )
-}
-export default RandomScreen
-
-
+const RandomScreen = (props: RandomScreenProps) => {
+  const {navigation} = props;
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <View style={{backgroundColor: 'red'}}></View>
+      <View style={styles.mainContainer}>
+        <WebView
+          source={{
+            uri: 'https://www.w3schools.com',
+          }}
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
+export default RandomScreen;
